686cca149af4c85dc1e93594de760ca19fe3e47b
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-09 04:16:49 +0500

    make top advisor page responsive

--- src/pages/reports/index.tsx
+++ src/pages/reports/index.tsx
@@ -1,42 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppLayout } from 'components/app/layout'
 import { Container } from 'components/app/container'
 import { DashboardCard } from 'components/dashboard/card'
-import personImage from 'assets/dashboard/person.png'
 import { AppFooter } from 'components/app/footer'
 import { AppHeader } from 'components/app/header'
+import { personIcon } from 'assets/icons'
+import { Notification } from 'components/app/notification'
+import clsx from 'clsx'
 
 export const Reports = () => {
 	const breadcrumbs = [
 		{ title: 'Reports', path: '/reports' },
 		{ title: 'Top Advisors', path: '/reports/top-advisors' }
 	]
+	const [status, setStatus] = useState(false)
+	const isNotificationOpen = (value = false) => {
+		setStatus(value)
+	}
+
 	return (
 		<AppLayout>
 			<AppHeader breadcrumbs={breadcrumbs} />
 			<Container>
 				<div className="pt-8 space-y-5">
 					<h2 className="font-semibold text-xl text-heading">Top Advisors</h2>
-					<div className="grid grid-cols-5 justify-around gap-y-7 gap-x-11">
-						<DashboardCard image={personImage} name="Advisor Name" status="Available" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Inactive" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Hired" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Hired" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Available" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Inactive" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Hired" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Hired" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Available" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Inactive" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Hired" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Hired" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Available" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Inactive" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Hired" />
-						<DashboardCard image={personImage} name="Advisor Name" status="Hired" />
+					<div
+						className={clsx(
+							'grid gap-y-7 gap-x-11',
+							status
+								? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6'
+								: 'grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-7'
+						)}>
+						<DashboardCard image={personIcon} name="Advisor Name" status="Available" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Inactive" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Hired" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Hired" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Available" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Inactive" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Hired" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Hired" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Available" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Inactive" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Hired" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Hired" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Available" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Inactive" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Hired" />
+						<DashboardCard image={personIcon} name="Advisor Name" status="Hired" />
 					</div>
 					<AppFooter />
 				</div>
+				<Notification getStatus={isNotificationOpen} />
 			</Container>
 		</AppLayout>
 	)
