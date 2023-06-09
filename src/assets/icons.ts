aa8d3894fac7b691cdcdf5c25e060f3cc452a128
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-09 09:59:28 +0500

    add logout svg

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,5 +1,7 @@
 import person from 'assets/pngs/person.png'
 import company from 'assets/pngs/company.png'
+import logout from 'assets/svgs/logout.svg'
 
 export const personIcon = person
 export const companyIcon = company
+export const logoutIcon = logout
