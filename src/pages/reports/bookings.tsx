46f25a76a8ccae520d76e21f166c980c9d75f1e3
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-18 02:59:31 +0500

    refactor booking page

--- src/pages/reports/bookings.tsx
+++ src/pages/reports/bookings.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
+import { toast } from 'react-hot-toast'
 import clsx from 'clsx'
 
 import { AppLayout } from 'components/app/layout'
 import { Container } from 'components/app/container'
 import { AppFooter } from 'components/app/footer'
 import { AppHeader } from 'components/app/header'
-import { Pagination } from 'components/app/pagination'
 import { companyIcon, personIcon } from 'assets/icons'
 import { Notification } from 'components/app/notification'
 import { InputDate } from 'components/fields/date'
-import { toast } from 'react-hot-toast'
+import { Table } from 'components/app/table'
 
 export const Bookings = () => {
 	const breadcrumbs = [
@@ -78,7 +78,7 @@ export const Bookings = () => {
 	const [selectedDate, setSelectedDate] = useState({ from: '', to: '' })
 
 	const onDateChange = (value: string, name: string) => {
-		if (name === 'from') setSelectedDate(prev => ({ ...prev, from: value }))
+		if (name === 'from') setSelectedDate(() => ({ to: '', from: value }))
 		if (name === 'to') {
 			if (new Date(value) > new Date(selectedDate.from)) {
 				setSelectedDate(prev => ({ ...prev, to: value }))
@@ -90,30 +90,28 @@ export const Bookings = () => {
 
 	const renderComponent = (items: any) => {
 		return (
-			<div className="space-y-4">
-				{items?.map((item: any, index: number) => (
-					<div
-						key={index}
-						className={clsx(
-							'border border-gray-300 p-6 grid grid-col-6 grid-flow-col items-center',
-							open ? 'gap-x-4' : 'gap-x-11'
-						)}>
-						<div className={clsx('flex items-center', open ? 'space-x-2' : 'space-x-4')}>
-							<img src={item?.image} alt="person" className="w-14 h-14" />
-							<p className="font-semibold text-sm">{item?.name}</p>
-						</div>
-						<p className="text-sm">{item?.price}</p>
-						<p className="text-sm">{item?.status}</p>
-						<p className="text-sm">{item?.price}</p>
-						<p className="text-sm">{item?.date}</p>
-						<p className="text-sm">{item?.title}</p>
-						<div className={clsx('flex items-center', open ? 'space-x-2' : 'space-x-4')}>
-							<img src={item?.company?.image} alt="person" className="w-14 h-14" />
-							<p className="font-semibold text-sm">{item?.company?.name}</p>
-						</div>
-					</div>
+			<tbody>
+				{items.map((item: any, index: number) => (
+					<tr key={index} className=" border">
+						<td className="tw-table-td pr-0">
+							<div className={clsx('flex items-center', open ? 'gap-x-2' : 'gap-x-4')}>
+								<img src={item.image} alt="person" className="w-14 h-14" />
+								<p className="font-semibold text-sm">{item.name}</p>
+							</div>
+						</td>
+						<td className="tw-table-td whitespace-nowrap">{item.price}</td>
+						<td className="tw-table-td">{item.status}</td>
+						<td className="tw-table-td">{item.date}</td>
+						<td className="tw-table-td">{item.title}</td>
+						<td className="tw-table-td pl-0">
+							<div className={clsx('flex items-center', open ? 'gap-x-2' : 'gap-x-4')}>
+								<img src={item.company.image} alt="person" className="w-14 h-14" />
+								<p className="text-sm">{item.company.name}</p>
+							</div>
+						</td>
+					</tr>
 				))}
-			</div>
+			</tbody>
 		)
 	}
 
@@ -135,13 +133,7 @@ export const Bookings = () => {
 							<InputDate value={selectedDate.to} label="To" name="to" onDateChange={onDateChange} />
 						</div>
 					</div>
-					<Pagination
-						totalSelectedPages={items?.length}
-						items={items}
-						renderComponent={renderComponent}
-						showButton={false}
-						showSelect={true}
-					/>
+					<Table items={items} renderComponent={renderComponent} showSelect={true} />
 					<AppFooter />
 				</div>
 				<Notification openNotifications={open} setOpenNotifications={setOpen} />
