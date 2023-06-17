00f7990add40b92c1fd928e57855b2d158b16aae
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-17 22:40:20 +0500

    add pdf icon

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,7 +1,9 @@
 import person from 'assets/pngs/person.png'
 import company from 'assets/pngs/company.png'
 import logout from 'assets/svgs/logout.svg'
+import pdf from 'assets/svgs/pdf.svg'
 
 export const personIcon = person
 export const companyIcon = company
 export const logoutIcon = logout
+export const pdfIcon = pdf
