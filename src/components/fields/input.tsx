077b28e4802ce8160eef49aa5904085ace77b2df
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-17 22:46:09 +0500

    add input component

---
+++ src/components/fields/input.tsx
@@ -0,0 +1,44 @@
+import clsx from 'clsx'
+import { InputHTMLAttributes } from 'react'
+import { UseFormRegister, FieldErrors, FieldValues } from 'react-hook-form'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	labelText?: string
+	register: UseFormRegister<any>
+	error?: FieldErrors<FieldValues>
+	name: string
+	className?: string
+}
+export const Input = ({
+	labelText,
+	name,
+	placeholder,
+	register,
+	error,
+	className,
+	...props
+}: InputProps) => {
+	const errorText = error?.[name]?.message as string
+
+	return (
+		<div className="w-full">
+			{labelText && (
+				<label htmlFor={name} className="block text-[#0D0C18]">
+					{labelText}
+				</label>
+			)}
+			<div>
+				<input
+					{...props}
+					{...(register?.(name) ?? {})}
+					placeholder={placeholder}
+					className={clsx(
+						'w-full text-ellipsis overflow-hidden bg-transparent placeholder-heading text-heading border border-transparent focus:outline-none active:bg-transparent',
+						className
+					)}
+				/>
+			</div>
+			{errorText && <p className="text-xs text-red-600 ">{errorText}</p>}
+		</div>
+	)
+}
